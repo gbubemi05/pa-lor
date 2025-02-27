5019ba9ef3e37fea2cad0101c56fe211a53ab323
gbubemi05 <165071754+gbubemi05@users.noreply.github.com>
2025-02-27 03:43:38 +0100

    added new features

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const btn = document.querySelector(".palette-btn");
-
+const popUp = document.querySelector(".pop-up");
 const btnPositive = document.querySelector(".positive-btn");
 const btnNegative = document.querySelector(".negative-btn");
 const paletteDiv = document.querySelector(".palette");
-
+const thisYear = document.querySelector(".current-year");
+const maxColorBoxes = 10;
+const minColorBoxes = 2;
 btnPositive.addEventListener("click", function () {
-  const html = `<div class="color-box"></div>`;
-  paletteDiv.insertAdjacentHTML("beforeend", html);
+  const colorBoxes = document.querySelectorAll(".color-box");
+  if (colorBoxes.length < maxColorBoxes) {
+    const html = `<div class="color-box"></div>`;
+    paletteDiv.insertAdjacentHTML("beforeend", html);
+  } else {
+    popUp.textContent = "you can only use 10 colors ";
+    popUp.style.opacity = "1";
+    setTimeout(function () {
+      popUp.style.opacity = "0";
+      popUp.textContent = popText; // Hide the div after 3 seconds
+    }, 3000);
+  }
 });
+
+btnNegative.addEventListener("click", function () {
+  remoneChild;
+});
+// btnPositive.addEventListener('click', function(){
+//   const html =`<div class="color-box"></div>`
+//   paletteDiv.
+// })
 function getRandomRGB() {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
@@ -41,6 +61,9 @@ btn.addEventListener("click", function () {
     const hexColor = getRandomHex();
     div.style.backgroundColor = hexColor;
     div.textContent = hexColor; // Add the hex code as text
+    div.addEventListener("click", function () {
+      copyToClipboard(hexColor);
+    });
   });
 });
 
@@ -49,8 +72,18 @@ function copyToClipboard(text) {
     .writeText(text)
     .then(() => {
       console.log("Text copied to clipboard");
+      // popUp.textContent = "you have reached the maximum number of colors ";
+      popUp.textContent = "color copied to clipboard";
+      popUp.style.opacity = "1";
+      setTimeout(function () {
+        popUp.style.opacity = "0"; // Hide the div after 3 seconds
+      }, 3000);
     })
     .catch((err) => {
       console.error("Could not copy text: ", err);
     });
 }
+
+const getFullYear = new Date().getFullYear();
+console.log(getFullYear);
+thisYear.textContent = getFullYear;
